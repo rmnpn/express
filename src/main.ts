71fd8bec2974ef6b2d4738b440aede1bdec597b2
rmnpn <romanpincak74@gmail.com>
2024-03-01 14:12:46 +0200

    twilio_aws

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import fileupload from "express-fileupload";
+import * as http from "http";
 import * as mongoose from "mongoose";
+import { Server } from "socket.io";
 import * as swaggerUi from "swagger-ui-express";
 
 import { configs } from "./configs/config";
@@ -10,13 +12,34 @@ import { adminRouter } from "./routers/admin.router";
 import { authRouter } from "./routers/auth.router";
 import { userRouter } from "./routers/user.router";
 import * as swaggerDocument from "./untils/swagger.json";
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+io.on("connection", (socket) => {
+  socket.on("click", () => {
+    console.log("hoba");
+
+    // socket.emit("peu peu");
+    // io.emit("all", "pdish");
+    // socket.broadcast.emit("allExceptMe", "VSI PDISH");
+  });
+  socket.on("room:join", ({ roomID }) => {
+    socket.join(roomID);
+
+    socket.to(roomID).emit("room:newUserJoined", socket.id);
+  });
+  socket.on("message:create", ({ data }) => {
+    io.emit("all", data);
+  });
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileupload());
 
 const PORT = configs.PORT;
-app.listen(PORT, async () => {
+server.listen(PORT, async () => {
   await mongoose.connect(configs.DB_URL);
   console.log("SERVER EBASHE");
 });
